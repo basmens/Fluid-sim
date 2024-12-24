7baae5b4ad3afb94648c93d6dccfc2e7cfed9f34
Bas Mens <B.Mens@student.tudelft.nl>
2024-12-24 21:54:33 +0100

    Now simulating gravity

--- Assets/Scripts/Simulation.cs
+++ Assets/Scripts/Simulation.cs
@@ -7,6 +7,13 @@ public class Simulation : MonoBehaviour
     public GameObject particlePrefab;
     public Vector3 size;
 
+    [Header("Time step")]
+    public float maxTimeStepFPS = 60;
+    public float iterationsPerFrame = 3;
+
+    [Header("Simulation")]
+    public Vector3 gravity = new Vector3(0, -9.81f, 0);
+
     Vector3[] particlePositions;
     Vector3[] particleVelocities;
     GameObject[] particles;
@@ -35,7 +42,54 @@ public class Simulation : MonoBehaviour
 
     void Update()
     {
-        // Debug.Log(particlePositions.Length);
+        float maxDt = maxTimeStepFPS > 0 ?  1f / maxTimeStepFPS : float.MaxValue;
+        float dt = Mathf.Min(Time.deltaTime, maxDt) / iterationsPerFrame;
+        for (int i = 0; i < iterationsPerFrame; i++) {
+            IterateSimulation(dt);
+        }
+
+        for (int i = 0; i < particles.Length; i++) {
+            particles[i].transform.localPosition = particlePositions[i];
+        }
+    }
+
+    void IterateSimulation(float dt) {
+        for (int i = 0; i < particlePositions.Length; i++) {
+            ref Vector3 pos = ref particlePositions[i];
+            ref Vector3 vel = ref particleVelocities[i];
+
+            vel += gravity * dt;
+            pos += vel * dt;
+
+            HandleBoundaryCollision(ref pos, ref vel);
+        }
+    }
+
+    void HandleBoundaryCollision(ref Vector3 pos, ref Vector3 vel) {
+        if (pos.x < -size.x / 2) {
+            pos.x = -size.x / 2;
+            vel.x = 0;
+        }
+        if (pos.x > size.x / 2) {
+            pos.x = size.x / 2;
+            vel.x = 0;
+        }
+        if (pos.y < -size.y / 2) {
+            pos.y = -size.y / 2;
+            vel.y = 0;
+        }
+        if (pos.y > size.y / 2) {
+            pos.y = size.y / 2;
+            vel.y = 0;
+        }
+        if (pos.z < -size.z / 2) {
+            pos.z = -size.z / 2;
+            vel.z = 0;
+        }
+        if (pos.z > size.z / 2) {
+            pos.z = size.z / 2;
+            vel.z = 0;
+        }
     }
 
     void OnDrawGizmos()
