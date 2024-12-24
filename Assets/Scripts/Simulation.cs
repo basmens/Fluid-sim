449af2418811efb517c015173d0838fe68c67760
Bas Mens <B.Mens@student.tudelft.nl>
2024-12-24 19:25:53 +0100

    Added gizmos to simulation

--- Assets/Scripts/Simulation.cs
+++ Assets/Scripts/Simulation.cs
@@ -25,17 +25,17 @@ public class Simulation : MonoBehaviour
         int i = 0;
         for (int x = 0; x < particlesPerAxis; x++)
         {
-            float px = (float) x / (particlesPerAxis - 1);
+            float px = (float)x / (particlesPerAxis - 1) - 0.5f;
             for (int y = 0; y < particlesPerAxis; y++)
             {
-                float py = (float) y / (particlesPerAxis - 1);
+                float py = (float)y / (particlesPerAxis - 1) - 0.5f;
                 for (int z = 0; z < particlesPerAxis; z++)
                 {
-                    float pz = (float) z / (particlesPerAxis - 1);
+                    float pz = (float)z / (particlesPerAxis - 1) - 0.5f;
                     particlePositions[i] = new Vector3(px, py, pz);
                     particleVelocities[i] = Vector3.zero;
 
-                    GameObject particle = Instantiate(particlePrefab, gameObject.transform);
+                    GameObject particle = Instantiate(particlePrefab, transform);
                     particle.transform.localPosition = particlePositions[i];
                     particles[i] = particle;
 
@@ -48,5 +48,15 @@ public class Simulation : MonoBehaviour
     void Update()
     {
         Debug.Log(particlePositions.Length);
+    }
+
+    void OnDrawGizmos()
+    {
+        // Draw Bounds
+        var m = Gizmos.matrix;
+        Gizmos.matrix = transform.localToWorldMatrix;
+        Gizmos.color = new Color(0, 1, 0, 0.5f);
+        Gizmos.DrawWireCube(Vector3.zero, Vector3.one);
+        Gizmos.matrix = m;
     }
 }
